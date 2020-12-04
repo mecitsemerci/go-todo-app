18bcbf18eae19b7900b7e73f334c0dc3edaa6589
Mecit Semerci <mecit@fireflyon.com>
2020-12-04 14:29:18 +0300

    Refactor controller file versioning

--- app/api/controller/provider.go
+++ app/api/controller/provider.go
@@ -1,13 +1,12 @@
 package controller
 
 import (
-	v1 "github.com/mecitsemerci/clean-go-todo-api/app/api/controller/v1"
 	"github.com/mecitsemerci/clean-go-todo-api/app/core/services"
 	"github.com/mecitsemerci/clean-go-todo-api/app/infra/repository/mongodb"
 )
 
-func ProvideTodoController(todoService services.ITodoService) v1.TodoController {
-	return v1.NewTodoController(todoService)
+func ProvideTodoController(todoService services.ITodoService) TodoControllerV1 {
+	return NewTodoController(todoService)
 }
 func ProvideHealthController(dbContext mongodb.DbContext) HealthController  {
 	return NewHealthController(dbContext)
